bccf94c60806b4e5b46d6a35a959d427a3a0517f
suyar <296399959@qq.com>
2022-07-19 11:45:18 +0800

    feat: 引入 less 包

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: false,
+        modifyVars: {
+          // hack: `true; @import '@less/config.less'`,
+        },
+      },
+    },
+  },
   plugins: [vue()]
 })
