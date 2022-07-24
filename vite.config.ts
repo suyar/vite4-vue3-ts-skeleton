bf8b68bf117217fef88437de6c2a37bdfab9c21f
suyar <296399959@qq.com>
2022-07-24 18:24:23 +0800

    feat: 优化代码格式化

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,5 +15,14 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
-});
+  plugins: [
+    vue(),
+    Components({
+      dirs: ['src/components'],
+      extensions: ['vue', 'ts'],
+      deep: true,
+      dts: false,
+      resolvers: [AntDesignVueResolver()],
+    }),
+  ],
+})
