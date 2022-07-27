7ae3d8fd2b5f8404a8caf5336fde31eff9432d0d
suyar <296399959@qq.com>
2022-07-27 16:02:28 +0800

    build: 增加 pinia 持久化插件

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).use(createPinia).mount('#app')
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+createApp(App).use(pinia).mount('#app')
